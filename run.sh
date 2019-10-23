207fa3129513c4fb5ea449305914d2c858c6dce8
Karissa Sticks <ksticks@ga-p4.21-school.ru>
2019-10-23 17:02:41 +0300

    update runner script

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 make
 EXE=./ksticks.filler
-#EXE="python3 proxy_to_file.py"
-EXE2=./ksticks.filler
-#cp $EXE $EXE2
-./resources/filler_vm -p1 "$EXE" -p2 "$EXE2" -f resources/maps/map01 # 2> stderr.txt
-echo "------------------stderr.txt-----------------------------"
-tail stderr.txt
+#EXE2=./ksticks.filler
+#EXE2=./resources/players/abanlin.filler #ez
+#EXE2=./resources/players/carli.filler  #ez
+#EXE2=./resources/players/champely.filler #not working
+#EXE2=./resources/players/grati.filler #ez
+#EXE2=./resources/players/hcao.filler #ez
+EXE2=./resources/players/superjeannot.filler  #ez
+./resources/filler_vm -p1 "$EXE" -p2 "$EXE2" -f resources/maps/map01
